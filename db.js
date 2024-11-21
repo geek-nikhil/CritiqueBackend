3f8185c59c7662478ce90ec5f69809291cfe3a6a
Nikhil Raikwar <126591020+geek-nikhil@users.noreply.github.com>
2024-11-22 01:19:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   console.log("object");
   try {
     // Fixed the space in the MongoDB URI
-    await mongoose.connect('mongodb+srv://nikhilraikwar846:IkQU5yIBMpFIluLs@cluster0.a8u4r.mongodb.net/critique');
+    await mongoose.connect('mongodb+srv://nikhilraikwar846:IkQU5yIBMpFIluLs@cluster0.a8u4r.mongodb.net/critiq');
     console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
